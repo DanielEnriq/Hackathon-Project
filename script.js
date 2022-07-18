a54f7a3e6f9f388a42ece6b319aa480fac8ded0d
pedroegit <pedroe@microsoft.com>
2022-07-18 06:23:54 -0700

    fixed box for making posts

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).on('click', 'div.barker button.follow', function(e) {
     console.log($(e.target).val());
 });
+
+// from "https://stackoverflow.com/questions/57942695/make-input-height-increase-when-typing"
+// adjusted to fit flow of program
+var textArea = document.getElementById("input-area");
+textArea.addEventListener("input", evt => {
+    textArea.style.height = textArea.scrollHeight + "px";
+ 
+});
